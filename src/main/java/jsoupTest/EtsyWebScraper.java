f322da410d26636239704f81c9dfca373f3ec7f3
Siham EL KHOUCHANI <sihamelkhouchani@gmail.com>
2023-02-26 15:06:48 +0100

    Extract all links

--- src/main/java/jsoupTest/EtsyWebScraper.java
+++ src/main/java/jsoupTest/EtsyWebScraper.java
@@ -3,6 +3,9 @@ package jsoupTest;
 import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.jsoup.nodes.Element;
+
 
 public class EtsyWebScraper {
 
@@ -11,8 +14,14 @@ public class EtsyWebScraper {
 		
 		final String url = "https://www.etsy.com";
 		Document Doc = Jsoup.connect(url).get();
-		System.out.println(Doc.title());
+		System.out.println("Title: "+Doc.title());
 		
+		Elements links = Doc.select("a[href]");
+		
+		for(Element link: links) {
+			System.out.println("\nlink :"+link.attr("href"));
+			System.out.println("text: "+link.text());
+		}
 	}
 
 }
